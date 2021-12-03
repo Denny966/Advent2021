a3ab7cf8cce2d9adfcf15bc5d1ed2b15ce3db019
Dennis <dennisp@scorito.com>
2021-12-03 13:00:13 +0100

    Day 3 part 2

--- advent3-pt2.js
+++ advent3-pt2.js
@@ -3,13 +3,13 @@ const text = fs.readFileSync("temp.txt", "utf-8");
 
 const split = text.split("\r\n");
 
-const result = [];
+let result = split.map((s) => s);
 
 for (let digitIndex = 0; digitIndex < 12; digitIndex++) {
     let bitZero = 0;
     let bitOne = 0;
-    for (let i = 0; i < split.length; i++) {
-        const digit = parseInt(split[i].charAt(digitIndex));
+    for (let i = 0; i < result.length; i++) {
+        const digit = parseInt(result[i].charAt(digitIndex));
         if (digit == 1) {
             bitOne++;
         }
@@ -17,19 +17,42 @@ for (let digitIndex = 0; digitIndex < 12; digitIndex++) {
             bitZero++;
         }
     }
-    if (bitOne > bitZero) {
-        result.push(1);
-    } else if (bitZero > bitOne) {
-        result.push(0);
+    if (bitZero > bitOne) {
+        result = result.filter((row) => parseInt(row.charAt(digitIndex)) == 0);
     } else {
-        result.push(2);
+        result = result.filter((row) => parseInt(row.charAt(digitIndex)) == 1);
     }
-    bitZero = 0;
-    bitOne = 0;
+    if (result.length === 1) {
+        break;
+    }
 }
 
-var gamma = parseInt(result.join(""), 2);
+const generatorRating = parseInt(result, 2);
 
-var epsilon = parseInt(result.map((r) => (r == 1 ? 0 : 1)).join(""), 2);
+result = split.map((s) => s);
 
-console.log(gamma * epsilon);
+for (let digitIndex = 0; digitIndex < 12; digitIndex++) {
+    let bitZero = 0;
+    let bitOne = 0;
+    for (let i = 0; i < result.length; i++) {
+        const digit = parseInt(result[i].charAt(digitIndex));
+        if (digit == 1) {
+            bitOne++;
+        }
+        if (digit == 0) {
+            bitZero++;
+        }
+    }
+    if (bitOne < bitZero) {
+        result = result.filter((row) => parseInt(row.charAt(digitIndex)) == 1);
+    } else {
+        result = result.filter((row) => parseInt(row.charAt(digitIndex)) == 0);
+    }
+    if (result.length === 1) {
+        break;
+    }
+}
+
+const scrubberRating = parseInt(result, 2);
+
+console.log(generatorRating * scrubberRating);
